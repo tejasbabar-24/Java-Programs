3e8cf31b05e5fba7f210694a3524090b027b9045
tejasbabar-24 <babartejas21@gmail.com>
2024-08-04 06:28:27 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+    public static void main(String[]args){
+        System.out.println("First Java Program");
+        printHello();
+    }
+
+    static void printHello()
+    {
+       System.out.printf("Hello");
+    }
+}
