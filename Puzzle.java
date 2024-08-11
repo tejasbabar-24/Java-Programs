32d94ae90e79aea9bbd1e0b219ad7babfeaadf20
tejasbabar-24 <babartejas21@gmail.com>
2024-08-11 05:07:37 -0700

    Add files via upload

---
+++ Puzzle.java
@@ -0,0 +1,7 @@
+public class Puzzle{
+    public static void main(String[] args){
+        System.out.println(5+"Java"+7+8);
+        System.out.println(5+1+"PGA"+9);
+        System.out.println("PGA"+(5+7)+"Java");
+    }
+}
