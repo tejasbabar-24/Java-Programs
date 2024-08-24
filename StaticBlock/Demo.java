efe71b6137624f0f4b53a98b0921a394ae235365
tejasbabar-24 <babartejas21@gmail.com>
2024-08-24 03:50:33 -0700

    Add files via upload

---
+++ StaticBlock/Demo.java
@@ -0,0 +1,11 @@
+/*Program to print name without writing anything inside main method*/
+class Demo{
+
+    static{
+        System.out.println("My name is Tejas");
+    }
+
+    public static void main(String[] args){
+
+    }
+}
