88659fd5a52880ced1987c07290a5f9ec7edb506
Tejas Vikas Babar <babartejas21@gmail.com>
2024-09-08 03:11:02 -0700

    Add files via upload

---
+++ String/CharCount.java
@@ -0,0 +1,10 @@
+package String;
+
+public class CharCount {
+    public static void main(String[] args) {
+        String str = "String Travers";
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+    }
+}
---
+++ String/StringPuzzle.java
@@ -0,0 +1,8 @@
+package String;
+
+public class StringPuzzle {
+    public static void main(String[] args) {
+        String str = "Hare Krishna Hare Rama !";
+
+    }
+}
