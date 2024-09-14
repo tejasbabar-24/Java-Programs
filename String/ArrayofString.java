9b8882a30f6ae1c39c7ff579db4da8da18031850
Tejas Vikas Babar <babartejas21@gmail.com>
2024-09-14 06:35:12 -0700

    Add files via upload

---
+++ String/ArrayofString.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class ArrayofString {
+
+    // Function to sort the array based on the length of strings
+    public static void sort(String[] arr) {
+        int size = arr.length;
+        for (int step = 0; step < size - 1; step++) {
+            for (int i = 0; i < size - step - 1; i++) {
+                if (arr[i].length() > arr[i + 1].length()) {
+                    // Swap the strings if they are out of order
+                    String temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Function to print the array
+
+    public static void main(String[] args) {
+        String[] arr = { "ram", "a", "ab" };
+        sort(arr);
+        int size = arr.length;
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+        System.out.println(); // To move to the next line after printing the array
+    }
+}
