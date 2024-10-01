7b23606402d55879d0168b09f0a02e43732b4b1b
Tejas Vikas Babar <babartejas21@gmail.com>
2024-10-01 10:24:54 -0700

    Add files via upload

---
+++ Exception/UserDefinedException.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class AgeInvalidException extends Exception {
+} // User defined exception class
+
+class UserDefinedException {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+        try {
+            validateAge(age);
+        } catch (AgeInvalidException e) {
+            System.out.println("Entered age is Invalid. Please pass age between 1-100");
+        }
+        System.out.println("****END****");
+    }
+
+    static void validateAge(int age) throws AgeInvalidException {
+        // 1-100
+        if (age >= 1 && age <= 100) {
+            System.out.println("Age is valid");
+        } else {
+            throw new AgeInvalidException();
+        }
+    }
+}
