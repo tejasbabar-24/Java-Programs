6fea7cb06f1d93ff054b0fa752f31d95184f991f
Tejas Vikas Babar <babartejas21@gmail.com>
2024-10-07 09:23:11 -0700

    Delete Collection/MapDemo.java

--- Collection/MapDemo.java
+++
@@ -1,8 +0,0 @@
-package Collection;
-import java.util.*;
-public class MapDemo {
-    public static void main(String[] args )
-    {
-        HashMapMap<String,Integer> map = new HashMap<>
-    }
-}
