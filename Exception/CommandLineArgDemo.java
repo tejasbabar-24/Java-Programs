946f092a0ff427426216298c3550f1dc41f1063f
Tejas Vikas Babar <babartejas21@gmail.com>
2024-10-17 22:17:55 +0530

    Add files via upload

---
+++ Exception/CommandLineArgDemo.java
@@ -0,0 +1,9 @@
+public class CommandLineArgDemo {
+
+    public static void main(String[] args) {
+        System.out.println(args.length);
+        int rollNo = Integer.parseInt(args[0]);
+        String name = args[1];
+        System.out.println("You have passed :" + rollNo + "," + name);
+    }
+}
