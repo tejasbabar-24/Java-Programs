f172cab7c4a72692988e2b3b16639a7617171eb1
Tejas Vikas Babar <babartejas21@gmail.com>
2024-10-31 21:40:52 +0530

    Add files via upload

---
+++ BZ_Lab/GuessingGame.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        int randomNumber = random.nextInt(100) + 1; // Random number between 1 and 100
+        int guess = 0;
+        int attempts = 0;
+
+        System.out.println("Welcome to the Guessing Game!");
+        System.out.println("I've chosen a number between 1 and 100. Try to guess it!");
+
+        while (guess != randomNumber) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < randomNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > randomNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+            }
+        }
+        
+        scanner.close();
+    }
+}
